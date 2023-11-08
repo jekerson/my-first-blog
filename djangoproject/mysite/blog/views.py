8260a093e923500127a852fd90b5bb988eeb2f4a
Rumi <doodlescia@gmail.com>
2023-11-08 13:39:45 +0300

    Changed the HTML for the site.

--- djangoproject/mysite/blog/views.py
+++ djangoproject/mysite/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    # Add an indented block here
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
